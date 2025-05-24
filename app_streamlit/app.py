60e4c3e3f297e2ded75a243a9c0d495e5aed9a88
Mwangiboyle <mwangiboyle4@gmail.com>
2025-05-24 10:34:50 +0300

    Added a file reader from langchain

--- app_streamlit/app.py
+++ app_streamlit/app.py
@@ -1,2 +1,30 @@
-import streamlit as st 
+import streamlit as st
+from langchain_community.document_loaders import PyPDFLoader
+import tempfile
+import os
 
+
+#app title
+st.title("Personal RAG APP")
+
+#file uploader
+uploaded_file = st.sidebar.file_uploader('upload your file', type=['docx', 'pdf', 'txt'])
+
+if uploaded_file is not None:
+    temp_dir = tempfile.TemporaryDirectory()
+    
+    temp_file_path = os.path.join(temp_dir.name, uploaded_file.name)
+    
+    with open(temp_file_path, "wb") as f:
+        f.write(uploaded_file.getbuffer())
+    
+    st.success(f"File saved to temporary directory: {temp_file_path}")
+
+
+    #read the pdf
+    path = str(temp_file_path)
+    loader = PyPDFLoader(temp_file_path)
+
+    document = loader.load()
+
+    st.write(document[0])
